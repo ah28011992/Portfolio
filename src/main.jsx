b5a7bafd00318b3ad74572a289f094c2982ad021
ah28011992 <hedgecockaaron@gmail.com>
2023-07-28 13:26:57 +0100

    added basic error handling and styling

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import ErrorPage from "./Components/ErrorPage.jsx";
 const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <App />,
+		errorElement: <ErrorPage />,
 	},
 ]);
 
