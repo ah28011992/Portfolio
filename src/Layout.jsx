24f825549dafa2d3a9e97c448bf8940876853137
ah28011992 <hedgecockaaron@gmail.com>
2023-07-30 21:41:52 +0100

    add footer section

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Header from "./Components/Header";
 import SideNav from "./Components/SideNav";
+import Footer from "./Components/Footer";
 import { Outlet } from "react-router-dom";
 
 const Layout = () => {
@@ -17,12 +18,17 @@ const Layout = () => {
 	return (
 		<div className='root-layout'>
 			{!isActive ? (
-				<div>
-					<Header openNav={openNav} />
-					<main className='main'>
-						<Outlet />
-					</main>
-				</div>
+				<>
+					<div>
+						<Header openNav={openNav} />
+						<main className='main'>
+							<Outlet />
+						</main>
+					</div>
+					<footer className='footer'>
+						<Footer />
+					</footer>
+				</>
 			) : (
 				<SideNav closeNav={closeNav} />
 			)}
