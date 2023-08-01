69bf65598c9a1941332ae63002c99e84fe646e71
ah28011992 <hedgecockaaron@gmail.com>
2023-08-01 14:17:38 +0100

    add button type drop down functionality

--- src/Components/Filter.jsx
+++ src/Components/Filter.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Filter = () => {
+	const [showContent, setShowContent] = useState(false);
+
+	const handleClick = () => {
+		setShowContent(!showContent);
+	};
+
 	return (
 		<section className='fitler-buttons'>
 			<button className='filter__btn'>All</button>
 			<button className='filter__btn'>Newest</button>
 			<button className='filter__btn'>Oldest</button>
 			<div className='dropdown'>
-				<button className='filter__btn'>Type &#9662;</button>
-				<div className='dropdown-content'>
+				<button
+					className='filter__btn'
+					onClick={handleClick}>
+					Type &#9662;
+				</button>
+				<div className={`dropdown-content ${showContent ? "show" : "hide"}`}>
 					<a
 						href='#'
 						className='dropdown__item'>
