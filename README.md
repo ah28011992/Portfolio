3e36d07c6a1dde5690e823ff64bdd73ae72c73cd
ah28011992 <hedgecockaaron@gmail.com>
2023-08-02 17:26:13 +0100

    add iweb git hub icons and links to to the project cards

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# WELCOME TO MY PORTFOLIO
+# WELCOME TO MY PORTFOLIO WEBAPP
+
+Hello there :) 
+
+My portfolio isn't hosted yet (it will be this friday 4th August)
+
+...If you cannot wait until then please feel free to : 
+
+1. Fork and clone the repo
+
+2. Run npm i OR yarn i 
+
+3. Run npm run dev to start the development server
+
+4. Checkout my code and feel free to leave constructive comments!
+
+I'm currently working on the projects page and commiting frequently so look out 
+for new changes! 
--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { portfolioObject, colorMapping } from "../data/projectData.js";
-
+import IconGitgub from "./icons/IconGitHub.jsx";
+import IconWeb from "./icons/IconWeb.jsx";
 const ProjectCard = () => {
 	return (
 		<section className='card'>
@@ -16,7 +17,29 @@ const ProjectCard = () => {
 						/>
 					</div>
 					<div className='card__item'>
-						<p>{item.name}</p>
+						<p className='card__name'>{item.name}</p>
+						<div className='icons icons-card'>
+							<div className='icon__list icon__list--card'>
+								<a
+									href={item.hostedWebLink}
+									target='_blank'
+									rel='noopener noreferrer'>
+									<button>
+										<IconWeb />
+									</button>
+								</a>
+							</div>
+							<div className='icon__list icon__list--card'>
+								<a
+									href={item.gitHubLink}
+									target='_blank'
+									rel='noopener noreferrer'>
+									<button>
+										<IconGitgub />
+									</button>
+								</a>
+							</div>
+						</div>
 					</div>
 					<div className='card__item'>
 						<p>{item.description}</p>
