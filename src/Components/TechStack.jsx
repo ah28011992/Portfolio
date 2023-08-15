0951b942c4dc4f0a9de9fcd08c9edf61081397c8
ah28011992 <hedgecockaaron@gmail.com>
2023-08-15 18:27:13 +0100

    fixed key error issue

--- src/Components/TechStack.jsx
+++ src/Components/TechStack.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { images } from "../stack-images";
 import Ball from "./Ball";
-import { key } from "localforage";
 
 const TechStack = () => {
 	return (
 		<section className='tech-stack'>
 			{images.map((img) => {
 				return (
-					<section className='tech-stack__content'>
-						<div key={img.name}>
+					<section
+						className='tech-stack__content'
+						key={img.name}>
+						<div>
 							<h3 className='tech-stack__text'>{img.name}</h3>
 							<Ball icon={img.img} />
 						</div>
