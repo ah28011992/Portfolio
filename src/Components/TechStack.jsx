71c08d0b5c2e0134fd1eeb21aaffc2743a6f7d86
ah28011992 <hedgecockaaron@gmail.com>
2023-08-15 11:08:03 +0100

    rendered 3d tech stack images with labels

--- src/Components/TechStack.jsx
+++ src/Components/TechStack.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { images } from "../stack-images";
+import Ball from "./Ball";
 
 const TechStack = () => {
-  return (
-    <div>TechStack</div>
-  )
-}
+	return (
+		<section className='techStack'>
+			{images.map((img) => {
+				return (
+					<>
+						<p>{img.name}</p>
+						<Ball
+							key={img.name}
+							icon={img.img}
+						/>
+					</>
+				);
+			})}
+		</section>
+	);
+};
 
-export default TechStack
+export default TechStack;
---
+++ src/stack-images.js
@@ -0,0 +1,35 @@
+import html from "./assets/img/html.png";
+import css from "./assets/img/css.png";
+import javascript from "./assets/img/javascript.png";
+import node from "./assets/img/node.png";
+import npm from "./assets/img/npm.png";
+import firebase from "./assets/img/firebase.png";
+
+const images = [
+	{
+		name: "html",
+		img: html,
+	},
+	{
+		name: "css",
+		img: css,
+	},
+	{
+		name: "javascript",
+		img: javascript,
+	},
+	{
+		name: "node",
+		img: node,
+	},
+	{
+		name: "npm",
+		img: npm,
+	},
+	{
+		name: "firebase",
+		img: firebase,
+	},
+];
+
+export { images };
