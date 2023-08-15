b770e1b192fc2c12f88f99f6b477446c90acd08e
ah28011992 <hedgecockaaron@gmail.com>
2023-08-15 11:50:57 +0100

    add stying to tech stack section

--- src/Components/TechStack.jsx
+++ src/Components/TechStack.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { images } from "../stack-images";
 import Ball from "./Ball";
+import { key } from "localforage";
 
 const TechStack = () => {
 	return (
-		<section className='techStack'>
+		<section className='tech-stack'>
 			{images.map((img) => {
 				return (
-					<>
-						<p>{img.name}</p>
-						<Ball
-							key={img.name}
-							icon={img.img}
-						/>
-					</>
+					<section className='tech-stack__content'>
+						<div key={img.name}>
+							<h3 className='tech-stack__text'>{img.name}</h3>
+							<Ball icon={img.img} />
+						</div>
+					</section>
 				);
 			})}
 		</section>
